d10df1519a7650caf4ec630664374ecc54047bf3
Kevin Costa <kcosta@Kevins-MacBook-Pro.local>
2016-12-02 22:06:07 +0100

    Commit

---
+++ Ft_ls/includes/ft_ls.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcosta <kcosta@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/02 14:51:17 by kcosta            #+#    #+#             */
+/*   Updated: 2016/12/02 17:01:38 by kcosta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <dirent.h>
+# include <sys/types.h>
+# include <sys/dir.h>
+# include <sys/stat.h>
+# include <errno.h>
+
+typedef struct dirent	t_dirent;
+typedef struct stat		t_stat;
+
+#endif
---
+++ Ft_ls/srcs/ft_ls.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcosta <kcosta@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/02 14:09:05 by kcosta            #+#    #+#             */
+/*   Updated: 2016/12/02 21:52:20 by kcosta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_ls.h"
+
+typedef struct dirent	t_dirent;
+
+static int	ft_usage(void)
+{
+	printf("usage: ./ft_ls directory file\n");
+	return (1);
+}
+
+static int	ft_error(int errnum, const char *filename)
+{
+	if (filename)
+		ft_printf("ft_ls: %s: %s\n", filename, strerror(errnum));
+	else
+		ft_printf("ft_ls: %s\n", strerror(errnum));
+	return (errnum);
+}
+
+static int	ft_list_dir(const char *filename)
+{
+	DIR			*dirp;
+	t_dirent	*entry;
+
+	if (!(dirp = opendir(filename)))
+		return (ft_error(errno, filename));
+	while ((entry = readdir(dirp)))
+	{
+		ft_printf("d_ino: %llu\nd_reclen: %hu\nd_type: %hhu\nd_namlen: %hu\n\
+d_name: %s\n",	entry->d_ino, entry->d_reclen, entry->d_type,
+				entry->d_namlen, entry->d_name);
+	}
+	if (closedir(dirp) == -1)
+		return (ft_error(errno, filename));
+	return (0);
+}
+
+static int	ft_inspect_file(const char *filename)
+{
+	t_stat	buf;
+
+	if (stat(filename, &buf) < 0)
+		return (ft_error(errno, filename));
+	//if (buf)
+	//{
+	ft_printf("st_dev: %d\nst_ino: %llu\nst_mode: %hu\nst_nlink: %hu\n\
+st_uid: %u\nst_gid: %u\nst_rdev: %d\nst_atimespec.tv_sec: %zu\n\
+st_mtimespec.tv_sec: %zu\nst_ctimespec.tv_sec: %zu\nst_size: %lld\n\
+st_blocks: %lld\nst_blksize: %d\nst_flags: %u\nst_gen: %u\n", buf.st_dev,
+buf.st_ino, buf.st_mode, buf.st_nlink, buf.st_uid, buf.st_gid, buf.st_rdev,
+			buf.st_atimespec.tv_sec, buf.st_mtimespec.tv_sec, buf.st_ctimespec.tv_sec,
+			buf.st_size, buf.st_blocks, buf.st_blksize, buf.st_flags, buf.st_gen);
+	ft_printf("%s%s%s", ctime(&buf.st_atimespec.tv_sec),
+			ctime(&buf.st_mtimespec.tv_sec),
+			ctime(&buf.st_ctimespec.tv_sec));
+
+	ft_printf((S_ISDIR(buf.st_mode)) ? "d" : "-");
+	ft_printf((buf.st_mode & S_IRUSR) ? "r" : "-");
+	ft_printf((buf.st_mode & S_IWUSR) ? "w" : "-");
+	ft_printf((buf.st_mode & S_IXUSR) ? "x" : "-");
+	ft_printf((buf.st_mode & S_IRGRP) ? "r" : "-");
+	ft_printf((buf.st_mode & S_IWGRP) ? "w" : "-");
+	ft_printf((buf.st_mode & S_IXGRP) ? "x" : "-");
+	ft_printf((buf.st_mode & S_IROTH) ? "r" : "-");
+	ft_printf((buf.st_mode & S_IWOTH) ? "w" : "-");
+	ft_printf((buf.st_mode & S_IXOTH) ? "x" : "-");
+	ft_printf("\n");
+	//}
+	//else
+	//	return (ft_error(errno, filename));
+	return (0);
+}
+
+int			main(int argc, char **argv)
+{
+	if (argc != 3)
+		return (ft_usage());
+	if (ft_list_dir(argv[1]))
+		return (2);
+	if (ft_inspect_file(argv[2]))
+		return (3);
+	return (0);
+}
