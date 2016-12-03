15a2ba97c29e9608808931dec1778d1eeabed49a
Kevin COSTA <kcosta@e3r7p10.42.fr>
2016-12-03 19:18:18 +0100

    Commit

---
+++ Ft_ls/srcs/error.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcosta <kcosta@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/03 18:43:57 by kcosta            #+#    #+#             */
+/*   Updated: 2016/12/03 18:45:58 by kcosta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+#include "libft.h"
+
+int		ft_error(int errnum, const char *filename)
+{
+	if (filename)
+		ft_printf("ft_ls: %s: %s\n", filename, strerror(errnum));
+	else
+		ft_printf("ft_ls: %s\n", strerror(errnum));
+	return (errnum);
+}
